dd0c3344b8fa0e8b730ea6be4881bf944ad34f5b
Muhammad-Salman-Hassan <msalmanhassan288311@gmail.com>
2025-06-20 12:30:16 +0500

    dummy data reduces count

--- src/util/DummyData.ts
+++ src/util/DummyData.ts
@@ -417,25 +417,7 @@ export const shareSeriesDummyData = [
     { label: "Time", value: "18/06/2025 14:57 (GMT+04:00)" },
     { label: "Currency", value: "AED" },
     { label: "Market", value: "Dubai" },
-    { label: "ISIN", value: "AEE01110S227" },
-    { label: "Industry", value: "Transportation" },
-    { label: "Symbol", value: "SALIK" },
-    { label: "Bid", value: 5.49 },
-    { label: "Ask", value: 5.54 },
-    { label: "Open", value: 5.59 },
-    { label: "Last", value: 5.5 },
-    { label: "Change +/-", value: -0.1 },
-    { label: "Change %", value: -1.79 },
-    { label: "High", value: 5.59 },
-    { label: "Low", value: 5.47 },
-    { label: "Volume", value: 6605615 },
-    { label: "Previous Close", value: 5.6 },
-    { label: "52 Weeks High", value: 5.99 },
-    { label: "52 Weeks Low", value: 3.26 },
-    { label: "YTD %", value: 3.7 },
-    { label: "52 Weeks %", value: 69.7 },
-    { label: "Number of Shares", value: 7500000000 },
-    { label: "Market Cap (AED Million)", value: 41250.0 },
+  
 ];
 
 export const sharePricelookupdummydata = [
